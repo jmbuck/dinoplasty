c4b29f8104cfe6193bbe22a2923c228b982d2859
Jordan Buckmaster <buckmast@purdue.edu>
2017-06-07 16:15:37 -0400

    Create dino object

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const app = {
         document
             .querySelector(formSelector)
             .addEventListener('submit', this.addDino.bind(this));
+        this.max = 0;
     
     },
 
     addDino(event) {
         event.preventDefault();
         const form = event.target;
-        const dinoName = form.dinoName.value;
+        const dino = { 
+            name: form.dinoName.value,
+            id: this.max+1,
+        };
+
+        console.log(dino.name, dino.id);
+        ++this.max;
     },
 
-}
+};
 
 app.init('#dino-form');
