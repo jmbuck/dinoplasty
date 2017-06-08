205655065bdef10c7340f58ce8b0e728d6c2dbd2
Jordan Buckmaster <buckmast@purdue.edu>
2017-06-08 00:23:51 -0400

    Implement like button and local storage of likes

--- app.js
+++ app.js
@@ -13,13 +13,16 @@ const app = {
     load() {
         this.names = localStorage.getItem('names');
         this.ids = localStorage.getItem('ids');
+        this.likes = localStorage.getItem('likes');
+        //If localStorage has data (isn't null), parse it and update list and data array
         if(this.names != null) {
             let namesArr = this.names.split('&');
             let idsArr = this.ids.split('&');
-            console.log('Test: '+namesArr[namesArr.length-1]);
+            let likesArr = this.likes.split('&');
             for(let i = 0; i < namesArr.length-1; i++) {
                 const dino = {name: namesArr[i],
-                            id: idsArr[i]}
+                            id: idsArr[i],
+                            liked: likesArr[i]};
                 this.dinos.push(dino);
                 this.list.insertBefore(this.renderListItem(dino), this.list.firstChild);
                 this.addEventListeners(dino.id);
@@ -27,40 +30,48 @@ const app = {
         } else {
             this.names = '';
             this.ids = '';
+            this.likes = '';
         }
     },
 
     updateStorage() {
         this.names = '';
         this.ids = '';
+        this.likes = '';
         for(let i = 0; i < this.dinos.length; i++) {
-            this.names +=  `${this.dinos[i].name}&`;
-            this.ids +=     `${this.dinos[i].id}&`;
+            this.names += `${this.dinos[i].name}&`;
+            this.ids += `${this.dinos[i].id}&`;
+            this.likes += `${this.dinos[i].liked}&`;
         }
         localStorage.setItem('names', this.names);
         localStorage.setItem('ids', this.ids);
+        localStorage.setItem('likes', this.likes);
     },
 
     renderListItem(dino) {
+        //Creates list item with unique IDs for each button
         const item = document.createElement('li');
         item.innerHTML =`
             <div class="input-group">
                 <div class="input-group-field">${dino.name}</div>
                 <div class="input-group-button">
-                    <button type="button" id="${'like'+dino.id}" class="button">Like</button>
+                    <button type="button" id="${'like'+dino.id}" class="button">${dino.liked == 'true' ? 'Unlike' : 'Like'}</button>
                 </div>
                 <div class="input-group-button">
                     <button type="button" id="${'del'+dino.id}" class="button">Delete</button>
                 </div>
                 <div class="input-group-button">
                     <button type="button" id="${'up'+dino.id}" class="button">Up</button>
-                </div>
+                </div>  
                 <div class="input-group-button">
                     <button type="button" id="${'down'+dino.id}" class="button">Down</button>
                 </div>
             </div>
         `;
         item.id = 'item'+dino.id;
+        if(dino.liked == 'true') {
+            item.style.backgroundColor = '#F0F3BD';
+        }
 
         return item;
     },
@@ -75,29 +86,43 @@ const app = {
     like(event) {
         const button = event.target;
         const listItem = document.querySelector(`#${button.id.replace('like','item')}`);
-        console.log(button.textContent);
-        if(button.textContent == 'Like') {
-            listItem.backgroundColor = '#F0F3BD';
-            button.textContent = 'Unlike';
-        } else {
-            listItem.backgroundColor = 'white';
-            button.textContent = 'Like';
+        let dino;
+        //Find dino in array
+        for(let i = 0; i < this.dinos.length; i++) {
+            if(this.dinos[i].id == listItem.id.substr(4)) {
+                dino = this.dinos[i];
+                break;
+            }
         }
+        //Toggles button text and background color
+        if(button.textContent == 'Like') {
+            listItem.style.backgroundColor = '#F0F3BD';
+            button.textContent = 'Unlike';
+            dino.liked = true;
+        } else {
+            listItem.style.backgroundColor = 'white';
+            button.textContent = 'Like';
+            dino.liked = false;
+        }
+        this.updateStorage();
     },
 
     delete(event) {
         const listItem = document.querySelector(`#${event.target.id.replace('del','item')}`);
+        //Searches the dinos array for id matching id # at end of listItem's id
         for(let i = 0; i < this.dinos.length; i++) {
             if(this.dinos[i].id == listItem.id.substr(4)) {
                 this.dinos.splice(i, 1);
                 this.updateStorage();
+                listItem.parentElement.removeChild(listItem);
             }
         }
-        listItem.parentElement.removeChild(listItem);
     },
 
     moveUp(event) {
         const listItem = document.querySelector(`#${event.target.id.replace('up','item')}`);
+        //Searches the dinos array for id matching id # at end of listItem's id
+        //and swaps the element with the next in the array (up in the list)
         for(let i = 0; i < this.dinos.length; i++) {
             if(this.dinos[i].id == listItem.id.substr(4)) {
                 if(i < this.dinos.length-1) {
@@ -114,6 +139,8 @@ const app = {
 
     moveDown(event) {
         const listItem = document.querySelector(`#${event.target.id.replace('down','item')}`);
+        //Searches the dinos array for id matching id # at end of listItem's id
+        //and swaps the element with the previous in the array (down in the list)
         for(let i = 0; i < this.dinos.length; i++) {
             if(this.dinos[i].id == listItem.id.substr(4)) {
                 if(i > 0) {
@@ -134,24 +161,27 @@ const app = {
         const dino = { 
             name: form.dinoName.value.trim(),
             id: this.max,
+            liked: false,
         };
-        form.dinoName.value = '';
+       
+        form.dinoName.value = ''; //Clears textbox
 
+        //Insert into start of list and dinos array
         this.list.insertBefore(this.renderListItem(dino), this.list.firstChild); 
         this.dinos.push(dino);
 
         this.addEventListeners(dino.id);
+
+        //Update names and ids string (for localStorage)
         this.names +=  `${dino.name}&`;
         this.ids +=     `${dino.id}&`;
+        this.likes +=  `${dino.liked}&`
         localStorage.setItem('names', this.names);
         localStorage.setItem('ids', this.ids);
+        localStorage.setItem('likes', this.likes);
 
         /*
             Add a promote/fav button 
-            Add a delete button
-            Add buttons to move a dinosaur up and down the list.
-            Persist the dinosaur data using `window.localStorage`. The dinosaurs should stay
-            in the list even when the page is refreshed.
             Edit button
         */
         this.max++;
